e9d0916ac7265d2ae1b947e6f08120b4268a1fe9
root <jhkwak@kisti.re.kr>
2019-05-21 17:35:09 +0900

    remove ambari-lustre directory

--- chameleon/ambari-lustre/YARNJOBMONITORSERVICE/package/scripts/params.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from resource_management import *
-
-# server configurations
-config = Script.get_config()
-
-#smoke_test_user = config['configurations']['lustrefs-config-env']['dummy_user']
-smoke_test_user = 'root'
